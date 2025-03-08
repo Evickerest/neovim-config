aee8c5d3051939f437cd5bb19b90e0f1766af181
Evickerest <awesomedragon23@gmail.com>
2025-03-08 12:24:39 -0500

    Added LSP features

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+-- Load plugins
+require("config.lazy")
+require("lualine").setup()
+require("luasnip").config.set_config({
+	enable_autosnippets = true,
+	store_selection_keys = "<Tab>"
+})
+
 -- General Configs
 local opt = vim.opt
 
@@ -11,16 +19,12 @@ opt.autochdir = true
 
 -- Keymaps
 local key = vim.keymap
-vim.g.mapleader = ''
+vim.g.mapleader = " "
 
 key.set("n", "<leader>p", vim.cmd.Ex)
 key.set("i", "<C-c>", "<Esc>")
 key.set("n", "<c-_>", "gcc")
 key.set("v", "<c-_>", "gc")
-
--- Load plugins
-require("config.lazy")
-require("lualine").setup()
 
 -- General
 vim.cmd("colorscheme catppuccin-mocha")
@@ -33,15 +37,4 @@ hi.nvim_set_hl(0, "LineNr", {fg = "White"})
 hi.nvim_set_hl(0, "LineNrAbove", {fg = "White"})
 hi.nvim_set_hl(0, "LineNrBelow", {fg = "White"})
 hi.nvim_set_hl(0, "Comment", {fg = "#989898"})
-
-
-
-
-
-
-
-
-
-
-
 
